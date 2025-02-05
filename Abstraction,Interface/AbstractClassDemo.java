247d91cdc120b8c912b42220a205135ef175844e
MonishaRajamanikkam <139474851+MonishaRajamanikkam@users.noreply.github.com>
2025-02-05 08:53:10 +0530

    Add files via upload

---
+++ Abstraction,Interface/AbstractClassDemo.java
@@ -0,0 +1,9 @@
+package tm;
+
+public class AbstractClassDemo {
+	 public static void main(String[] args) {
+	        // Creating an object of Dog class
+	        Animal myDog = new Dog();
+	        myDog.makeSound(); // Calls the overridden method in Dog
+	    }
+}
