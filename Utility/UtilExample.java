43d97d188c5cfcd2ecb13f7dfa5df2e1cec48e80
MonishaRajamanikkam <139474851+MonishaRajamanikkam@users.noreply.github.com>
2025-02-05 09:07:06 +0530

    Add files via upload

---
+++ Utility/UtilExample.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class UtilExample {
+	 public static void main(String[] args) {
+	        int number = -98765;
+	        int length = NumberUtils.getIntegerLength(number);
+	        
+	        System.out.println("Length of " + number + " is " + length);
+	    }
+}
