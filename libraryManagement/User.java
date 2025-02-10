c936d2a5bd187bed949bbd2e101f99d73c42deb4
MonishaRajamanikkam <139474851+MonishaRajamanikkam@users.noreply.github.com>
2025-02-10 20:03:55 +0530

    Add files via upload

---
+++ libraryManagement/User.java
@@ -0,0 +1,40 @@
+package libraryManagement;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+public class User implements Serializable {
+	    private String name;
+	    private String userID;
+	    private List<Book> borrowedBooks;
+
+	    public User(String name, String userID) {
+	        this.name = name;
+	        this.userID = userID;
+	        this.borrowedBooks = new ArrayList<>();
+	    }
+
+	    public String getName() { return name; }
+	    public String getUserID() { return userID; }
+	    public List<Book> getBorrowedBooks() { return borrowedBooks; }
+
+	    public void borrowBook(Book book) { borrowedBooks.add(book); }
+	    public void returnBook(Book book) { borrowedBooks.remove(book); }
+
+	    @Override
+	    public String toString() {
+	        return name + " (ID: " + userID + ")";
+	    }
+	}
+class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message) { super(message); }
+}
+
+class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) { super(message); }
+}
+
+class MaxBooksAllowedException extends Exception {
+    public MaxBooksAllowedException(String message) { super(message); }
+}
+
+
